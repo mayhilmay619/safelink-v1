54df40e15f79f74bf165820f69e0937a2cf04dc7
mayhilmay619 <36109690+mayhilmay619@users.noreply.github.com>
2018-03-23 09:44:55 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,10 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="secure-links.html"}
-if(x=="2"){xxx="search-and-encrypt-link.html"}
-if(x=="3"){xxx="generate-url.html"}
-if(x=="4"){xxx="encrypt-your-link.html"}
-if(x=="5"){xxx="go-to-url.html"}a_to_ve[i].href="https://linkkuaman.blogspot.com/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="go-to-url.html"}a_to_ve[i].href="https://linkkuaman.blogspot.com/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
